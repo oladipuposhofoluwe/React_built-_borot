34383993c92b3212af61f0c7d1af98f3546d4683
oladipuposhofoluwe <oladipuposhofoluwe@gmail.com>
2021-06-22 11:27:57 +0100

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,75 @@
+// export const robots = [
+//     {
+//         id: 1,
+//         name: 'Ervim Howell',
+//         username: 'Ervim',
+//         email: 'shanna@mellina.tv'
+//     },
+//     {
+//         id: 2,
+//         name: 'John Doe',
+//         username: 'John',
+//         email: 'johndoe@gmail.com'
+//     },
+//     {
+//         id: 3,
+//         name: 'Mariam Lambert',
+//         username: 'lamber$real',
+//         email: 'mariamlambert@yahoo.com'
+//     },
+//     {
+//         id: 4,
+//         name: 'Nicholas Bendtner',
+//         username: 'Nichol',
+//         email: 'bendtnerbb@gmail.com'
+//     },
+//     {
+//         id: 5,
+//         name: 'Kurtis Weissant',
+//         username: 'flyn',
+//         email: 'kurtisweissant@gmail.com'
+//     },
+//     {
+//         id: 6,
+//         name: 'Shola Aliyu',
+//         username: 'Shola',
+//         email: 'sholaaliyu@gmail.com'
+//     },
+//     {
+//         id: 7,
+//         name: 'Jaiye Kuti ',
+//         username: 'jaiyeola',
+//         email: 'jaiyekuti@gmail.com'
+//     },
+//     {
+//         id: 8,
+//         name: 'Shola Aliyu',
+//         username: 'Shola',
+//         email: 'sholaaliyu@gmail.com'
+//     },
+//     {
+//         id: 9,
+//         name: 'Kola Oyewole',
+//         username: 'Oyewole',
+//         email: 'kolaoyawole@gmail.com'
+//     },
+//     {
+//         id: 10,
+//         name: 'Dmilola Adenusi',
+//         username: 'dammy123',
+//         email: 'adenusi@gmail.com'
+//     },
+    
+//     {
+//         id: 11,
+//         name: 'Azeez Sholanke ',
+//         username: 'azeezu',
+//         email: 'azeezsholanke@gmail.com'
+//     },
+//     {
+//         id: 12,
+//         name: 'Ola Dipson',
+//         username: 'dipson546',
+//         email: 'oladipson@gmail.com'
+//     },
+// ]
